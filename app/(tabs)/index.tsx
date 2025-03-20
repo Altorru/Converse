e95bf8bb4b8249ecc65e66ca877113d424e7d42c
Hugo VIAUD <hugoviaud85@gmail.com>
2025-03-20 22:54:33 +0100

    💄 Update layout: increase marginTop in tabs and settings for improved spacing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     padding: 5,
-    marginTop: 20,
+    marginTop: 30,
   },
   titleContainer: {
     flexDirection: "row",
